b993566af404774efa42651dac64a88f04ca93b7
Denzy7 <dennismyner7@gmail.com>
2023-02-07 16:35:03 +0300

    open basic camera + mlkit deps

--- app/src/main/java/com/denzygames/mobilepos/CameraScan.kt
+++ app/src/main/java/com/denzygames/mobilepos/CameraScan.kt
@@ -4,11 +4,24 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
+import androidx.camera.camera2.internal.annotation.CameraExecutor
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.denzygames.mobilepos.databinding.ActivityCameraScanBinding
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraScan : AppCompatActivity() {
 
+    private lateinit var viewBinding: ActivityCameraScanBinding
+    private lateinit var barcodeScanner: BarcodeScanner
+    private lateinit var cameraExecutor: ExecutorService
     companion object
     {
         private val NEEDED_PERMISSIONS =
@@ -38,7 +51,8 @@ class CameraScan : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_camera_scan)
+        viewBinding = ActivityCameraScanBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
 
         if(permissionCameraGranted())
         {
@@ -47,6 +61,8 @@ class CameraScan : AppCompatActivity() {
         {
             needCameraPermissionDialog()
         }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     override fun onRequestPermissionsResult(
@@ -80,6 +96,24 @@ class CameraScan : AppCompatActivity() {
 
     fun startCameraScan()
     {
+        var cameraController = LifecycleCameraController(baseContext)
+        val preview: PreviewView = viewBinding.previewView
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
+            .build()
 
+        barcodeScanner = BarcodeScanning.getClient(options)
+//        cameraController.setImageAnalysisAnalyzer(
+//            ContextCompat.getMainExecutor(this),
+//            ...
+//        )
+        cameraController.bindToLifecycle(this)
+        preview.controller = cameraController
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+        barcodeScanner.close()
     }
 }
