527a87c03acda2a185b379423135553071028a37
Denzy7 <dennismyner7@gmail.com>
2023-02-07 11:23:30 +0300

    products UI + start its activity

--- app/src/main/java/com/denzygames/mobilepos/MainActivity.kt
+++ app/src/main/java/com/denzygames/mobilepos/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.denzygames.mobilepos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Toast
+import com.denzygames.mobilepos.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.btProducts.setOnClickListener{
+            Toast.makeText(this,"Launch Products", Toast.LENGTH_SHORT).show()
+            val intent = Intent(it.context,Products::class.java)
+            startActivity(intent)
+        }
     }
 }
